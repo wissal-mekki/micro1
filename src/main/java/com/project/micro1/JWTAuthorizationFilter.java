a545b05078deb500f80e2a7a62f0fe67e50a384b
wissal-mekki <48435711+wissal-mekki@users.noreply.github.com>
2019-10-03 02:48:48 +0100

    Update JWTAuthorizationFilter.java

--- src/main/java/com/project/micro1/JWTAuthorizationFilter.java
+++ src/main/java/com/project/micro1/JWTAuthorizationFilter.java
@@ -1,5 +1,5 @@
 package com.project.micro1;
-
+import static com.sun.xml.internal.ws.api.message.Packet.Status.Request;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
