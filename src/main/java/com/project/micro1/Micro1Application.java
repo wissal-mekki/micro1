5d705d84313be224096b0797185272bb44309581
wissal-mekki <48435711+wissal-mekki@users.noreply.github.com>
2019-10-03 02:54:33 +0100

    Update Micro1Application.java

--- src/main/java/com/project/micro1/Micro1Application.java
+++ src/main/java/com/project/micro1/Micro1Application.java
@@ -12,7 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import sun.java2d.pipe.hw.AccelDeviceEventListener;
 
 @SpringBootApplication
 public class Micro1Application implements CommandLineRunner {
@@ -20,7 +19,7 @@ public class Micro1Application implements CommandLineRunner {
 	private ObjectMapper objectMapper;
 	@Autowired
 	private AccountService accountService ;
- // private  BCryptPasswordEncoder bCryptPasswordEncoder ;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Micro1Application.class, args);
 	}
@@ -31,15 +30,7 @@ public class Micro1Application implements CommandLineRunner {
     }
 	@Override
 	public void run(String... arg0) throws Exception {
-/*		accountService.save(new Role(null,"USER"));
-		accountService.save(new Role(null,"ADMIN"));
-	//
-	//	accountService.saveUser(new User(null,null,null,null,null,null,null,"admin","1234", null));
-		accountService.addRoleToUser("user", "USER");
-		accountService.addRoleToUser("admin", "USER");
-		accountService.addRoleToUser("admin", "ADMIN");*/
-	//	accountService.saveUser(new User("wass","mk","22/22/22",12345678,"mmlkk","p@p.p",12345678,"wass","1234", null));
-	//	accountService.addRoleToUser("wass", "USER");
+
 	}
 
 	@PostConstruct
